274fbb461899ae88349f358490b2bb01f3931180
hadinurr7 <153978165+hadinurr7@users.noreply.github.com>
2025-01-04 10:41:54 +0700

    Change password (#14)
    * profile
    
    * feat: change-password

--- src/app/profile/change-password/page.tsx
+++ src/app/profile/change-password/page.tsx
@@ -1,4 +1,4 @@
-import ProfilePage from "@/features/profile";
+
 import { ChangePasswordForm } from "@/features/profile/change-password";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
