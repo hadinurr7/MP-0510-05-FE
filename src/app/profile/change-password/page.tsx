89c72ad5d32eef82d17e1bd83aa87e61f192d9a6
hadinurr7 <hadinurr7@gmail.com>
2025-01-04 10:38:26 +0700

    feat: change-password

--- src/app/profile/change-password/page.tsx
+++ src/app/profile/change-password/page.tsx
@@ -1,4 +1,3 @@
-import ProfilePage from "@/features/profile";
 import { ChangePasswordForm } from "@/features/profile/change-password";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
