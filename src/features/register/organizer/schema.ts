58d8937d7a3aa270545632ee9402c99a1120b8a6
hadinurr7 <hadinurr7@gmail.com>
2025-01-04 08:20:49 +0700

    fix: register

---
+++ src/features/register/organizer/schema.ts
@@ -0,0 +1,20 @@
+import * as Yup from "yup";
+import YupPassword from "yup-password";
+YupPassword(Yup);
+
+export const RegisterSchema = Yup.object().shape({
+  fullname: Yup.string().required("Fullname is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string()
+    .required("Password is required")
+    .minLowercase(1)
+    .minNumbers(1)
+    .minUppercase(1)
+    .min(6),
+  phoneNumber: Yup.string()
+    .matches(/^[0-9]{11,13}$/, "Phone number must be between 11 and 13 digits")
+    .required("Phone Number is Required"),
+  confirmPassword: Yup.string()
+    .required("Confirm Password is required")
+    .oneOf([Yup.ref("password")], "Password must match"),
+});
