7830c2685b9c4ac1287090e640ba3af16e5cba56
hikwoa-hfw <hibbanhfw@gmail.com>
2025-01-08 13:20:55 +0700

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,55 @@
+"use client";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
 
 const Navbar: React.FC = () => {
+  const { data } = useSession();
+  const user = data?.user;
+
+  const logout = () => {
+    signOut();
+  };
   return (
     <nav className="bg-gray-900 px-6 py-4 text-white">
       <div className="container mx-auto flex items-center justify-between">
         <a href="/" className="text-2xl font-bold">
-          Ticketer
+          TicketerHH
         </a>
         <div className="flex items-center space-x-4">
-          <a href="/concerts" className="hover:text-gray-400">
-            Concerts
-          </a>
-          <a
-            href="/register"
-            className="rounded bg-blue-500 px-4 py-2 hover:bg-blue-600"
-          >
-            Register
-          </a>
-          <a
-            href="/login"
-            className="rounded bg-blue-500 px-4 py-2 hover:bg-blue-600"
-          >
-            Login
-          </a>
+          
+          {!user?.id ? (
+            <Link href="/login">
+              <Button className="rounded-full bg-blue-500 px-6 py-2 text-white shadow-md transition-all hover:bg-blue-600">
+                Sign in
+              </Button>
+            </Link>
+          ) : (
+            <>
+              {user?.role === "CUSTOMER" ? (
+                <Link
+                  href="/profile/transaction-history"
+                  className="transition-colors hover:text-purple-600"
+                >
+                  Profile
+                </Link>
+              ) : (
+                <Link
+                  href="/dashboard"
+                  className="transition-colors hover:text-purple-600"
+                >
+                  Dashboard
+                </Link>
+              )}
+              <p
+                onClick={logout}
+                className="cursor-pointer text-red-600 transition-colors hover:text-red-800"
+              >
+                Logout
+              </p>
+            </>
+          )}
         </div>
       </div>
     </nav>
